6e2764c61b43320d346a92d21910fd47aaed21c9
gzy02 <67055520+gzy02@users.noreply.github.com>
2022-05-05 00:08:12 +0800

    Delete include directory

--- include/local_ip_mac_info.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#define MAC_SIZE 18
-#define IP_SIZE 16
-
-extern int get_ip_by_domain(const char *domain, char *ip); // 根据域名获取ip
-extern int get_local_mac(const char *eth_inf, char *mac);  // 获取本机mac
-extern int get_local_ip(const char *eth_inf, char *ip);    // 获取本机ip
--- include/my_daemon.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-void my_daemon(int nochdir, int noclose);
-void ERR_EXIT(const char *m);
